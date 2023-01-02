dd880dacb925d2d871a5d1b52a8cbc90db4fe3ca
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-01-02 13:03:10 +0530

    oop1

---
+++ oop1.dart
@@ -0,0 +1,26 @@
+class Person {
+  String name = 'Jiya';
+  int age = 10;
+  double weight = 21.25;
+  bool gender = true;
+  void display() {
+    print("Name = $name");
+    print("age = $age");
+    print("weight = $weight");
+    print("gender = $gender");
+  }
+}
+
+void main() {
+  //classname object = new classname();
+  Person p1 = new Person();
+  p1.display();
+
+  p1.name = "Jiya Ankit Patel";
+  p1.age = 11;
+  p1.weight = 25.50;
+
+  
+  p1.display();
+
+}
